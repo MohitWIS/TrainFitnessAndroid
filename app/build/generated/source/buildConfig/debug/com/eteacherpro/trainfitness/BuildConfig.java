180817b67f0d84e46e3ce2e656092a8521bc9eb6
MohitWIS <mohit.saxena@wisethink.in>
2021-05-07 14:27:37 +0530

    11.04.01 commit

---
+++ app/build/generated/source/buildConfig/debug/com/eteacherpro/trainfitness/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.eteacherpro.trainfitness;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.eteacherpro.trainfitness";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 800300012;
+  public static final String VERSION_NAME = "11.04";
+}
---
+++ app/build/generated/source/buildConfig/release/com/eteacherpro/trainfitness/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.eteacherpro.trainfitness;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.eteacherpro.trainfitness";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 800200012;
+  public static final String VERSION_NAME = "11.03";
+}
